5a8302c2bdd5a3a9d4784fd5d52778e180e8e1fa
Muhamad Nugroho Eko Saputro <mnugrohoe@gmail.com>
2023-06-04 22:39:42 +0700

    fix save on web

--- app/StickerSmash/index.js
+++ app/StickerSmash/index.js
@@ -1,22 +1,28 @@
-import { View } from "react-native";
-import color from "../../libs/colors";
-import PlaceHolderImage from "../../assets/images/bg-image.jpg";
-import ImageViewer from "./components/ImageViewer";
-import Button from "./components/Button";
-import IconButton from "./components/IconButton";
-import CircleButton from "./components/CircleButton";
+import domtoimage from "dom-to-image";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
-import EmojiPicker from "./components/EmojiPicker";
-import EmojiList from "./components/EmojiList";
-import EmojiSticker from "./components/EmojiSticker";
+import * as MediaLibrary from "expo-media-library";
+import { useRef, useState } from "react";
+import { Platform, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
+import PlaceHolderImage from "../../assets/images/bg-image.jpg";
+import color from "../../libs/colors";
+import Button from "./components/Button";
+import CircleButton from "./components/CircleButton";
+import EmojiList from "./components/EmojiList";
+import EmojiPicker from "./components/EmojiPicker";
+import EmojiSticker from "./components/EmojiSticker";
+import IconButton from "./components/IconButton";
+import ImageViewer from "./components/ImageViewer";
 
 export default function StickerSmash() {
+  const imageRef = useRef();
+
   const [selectedImage, setSelectedImage] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
 
   const pickImageAsync = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -45,8 +51,42 @@ export default function StickerSmash() {
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    if (Platform.OS === "web") {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+      return;
+    }
+
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  if (status === null) {
+    requestPermission();
+  }
 
   return (
     <GestureHandlerRootView
@@ -60,13 +100,15 @@ export default function StickerSmash() {
       }}
     >
       <View style={{ flex: 1, paddingTop: 58 }}>
-        <ImageViewer
-          PlaceHolderImage={PlaceHolderImage}
-          SelectedImage={selectedImage}
-        />
-        {pickedEmoji !== null ? (
-          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-        ) : null}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            PlaceHolderImage={PlaceHolderImage}
+            SelectedImage={selectedImage}
+          />
+          {pickedEmoji !== null ? (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          ) : null}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={{ position: "absolute", bottom: 80 }}>
